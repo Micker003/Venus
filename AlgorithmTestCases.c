546c2b81245b55f18986a5b8fe35f492e5e724ce
sansh <s.malhotra1@student.tue.nl>
2024-05-17 09:30:54 +0200

    Created new file "AlgorithmTestCases" to store all of the test cases for the methods in the BFStest file.

---
+++ AlgorithmTestCases.c
@@ -0,0 +1,10 @@
+#include "BFStest.c"
+#include "embeddedInitialImp.c"
+
+/**
+ * File with test cases to test each of the methods inside of the algorithm.
+ * For each test method specify input and expected output in comments above method. 
+ * run a test for each method and ensure that the method performs within the use case. 
+ * create test cases to verify the arraylist and queue as well. 
+*/
+
