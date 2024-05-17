1b4135a37bf953b2d70b2b4804a5dbafcc977834
Chi <Cycolahyper@gmail.com>
2024-05-17 18:51:47 +0200

    Changed parameter name, reduced time delay between measurements
    sleep_msec might be removed upon testing if measurements without it is still accurate.

--- IR_sensor.c
+++ IR_sensor.c
@@ -5,19 +5,22 @@
 #define ROLLING_AVERAGE  10
 #define ADC_VOLTAGE_5V  65535
 
+void IR_init(){
+  adc_init();
+}
 
-int IR_read(adc_channel_t IR_pin){
+int IR_read(adc_channel_t adc_channel){
   int count = 0; //counter for rolling average
   int ADC_plus,ADC_minus,average_IR_value,IR_value,total_IR_value = 0;
   
   //read IR values and add to total
   while (count != ROLLING_AVERAGE){    
       ADC_plus = ADC_VOLTAGE_5V;
-      ADC_minus = adc_read_channel_raw(IR_pin);
+      ADC_minus = adc_read_channel_raw(adc_channel);
       IR_value = ADC_plus - ADC_minus;
       total_IR_value +=IR_value;
       count ++;
-      sleep_msec(100);
+      sleep_msec(1);
   }
   //calculate average IR value
   average_IR_value = total_IR_value/ROLLING_AVERAGE;
