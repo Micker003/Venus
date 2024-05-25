4905e1f2e7a9186596774dc908863d3d42e81133
Chi <Cycolahyper@gmail.com>
2024-05-25 15:51:34 +0200

    Updated to work on only 1 IIC channel
    At the beginning, supply power to 1 sensor. Supply power to the 2nd sensor only after the first has been initialized (follow instructions on terminal)

--- distance.h
+++ distance.h
@@ -1,39 +1,92 @@
-#ifndef VL53L0X_H
-#define VL53L0X_H
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <libpynq.h>
 
-#define VL53L0X_OUT_OF_RANGE (8190)
+#ifndef _TOFLIB_H_
+#define _TOFLIB_H_
 
 
-/**
- * Initialize forward distance sensor
- * Initialize the followings before usage:
- *      iic_init(IIC0);
- *      switchbox_set_pin(IO_AR_SDA , SWB_IIC0_SDA);
- *      switchbox_set_pin(IO_AR_SCL , SWB_IIC0_SCL);
-*/
-bool forward_distance_init();
-
-/**
- * Initialize downward distance sensor
- * Initialize the followings before usage:
- *      iic_init(IIC1);
- *      switchbox_set_pin(IO_AR4, SWB_IIC1_SDA);
- *      switchbox_set_pin(IO_AR5, SWB_IIC1_SCL);
-*/
-bool downward_distance_init();
-
-/**
- * Does a single range measurement
- * @param iic selects specific iic channel
- * @param range contains the measured range or VL53L0X_OUT_OF_RANGE
- *        if out of range.
- * @return True if success, False if error
- * @note   Polling-based
+ /*
+ *  TU/e 5EID0::LIBPYNQ Driver for VL53L0X TOF Sensor
+ *
+ *  Original C library: Larry Bank
+ *  Adapted for PYNQ: Walthzer
+ * 
  */
-bool vl53l0x_read_range_single(uint16_t *range, const iic_index_t iic);
 
-#endif /* VL53L0X_H */
+//
+// Copyright (c) 2017 Larry Bank
+// email: bitbank@pobox.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/**
+ * @brief Internal type, do not modify directly. 
+ */
+typedef struct _vl53_sensor_ {
+    iic_index_t iic_index;
+    uint8_t baseAddr;
+    uint8_t stop_variable;
+    uint32_t measurement_timing_budget_us;
+} vl53x;
+
+/**
+ * @brief Set IIC address of a VL53L0X Sensor
+ * @param addr IIC Address of the sensor
+ * @param iic IIC Index
+ * @param newAddr IIC Target Address of the sensor
+ * @return 0 if successful, 1 on error
+ */
+extern int tofSetAddress(iic_index_t iic, uint8_t addr, uint8_t newAddr);
+
+/**
+ * @brief Connection test for a VL53L0X Sensor
+ * @note USE BEFORE `tofInit`!
+ * @param addr IIC Address of the sensor
+ * @param iic IIC Index
+ * @return 0 if successful, 1 on error
+ */
+extern int tofPing(iic_index_t iic, uint8_t addr);
+
+/**
+ * @brief Initialize the VL53L0X Sensor
+ * @param sensor Handle to the sensor.
+ * @param iic IIC Index
+ * @param addr IIC Address of the sensor
+ * @param bLongRange 0 => default mode (30-800mm) or 1 => long range mode (30-2000mm)
+ * @return 0 if successful, 1 on error
+ */
+extern int tofInit(vl53x *sensor, iic_index_t iic, uint8_t addr, int bLongRange);
+
+extern int multipleInit(vl53x *sensorA, vl53x *sensorB);
+
+/**
+ * @brief Read the model and revision of the VL53L0X Sensor
+ * @param sensor Handle to the sensor.
+ * @param model pointer to store model
+ * @param revision pointer to store revision
+ * @return 0 if successful, 1 on error
+ */
+extern int tofGetModel(vl53x *sensor, uint8_t *model, uint8_t *revision);
+
+/**
+ * @brief Read current distance in mm
+ * @param sensor Handle to the sensor.
+ * @returns distance in mm
+ */
+extern uint32_t tofReadDistance(vl53x *sensor);
+
+
+#endif // _TOFLIB_H
