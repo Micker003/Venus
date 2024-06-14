c8f9c80d481f7f50198b6839f89a8ac3b6a9d42f
Micker003 <43570163+Micker003@users.noreply.github.com>
2024-06-14 12:27:05 +0200

    Update messageSendTest.c

--- messageSendTest.c
+++ messageSendTest.c
@@ -16,6 +16,19 @@ int main(void) {
     stepper_init();
     stepper_enable();
     embeddedInit();
+
+    switchbox_init();
+    switchbox_set_pin(IO_AR0, SWB_UART0_RX);
+    switchbox_set_pin(IO_AR1, SWB_UART0_TX);
+
+    // initialize UART 0
+    uart_init(UART0);
+    // flush FIFOs of UART 0
+    uart_reset_fifos(UART0);
+
+    // char buf[256];
+    fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
+
     uart_init(UART0);
     send_information(0,2,2);
     send_information(2,3,3);
